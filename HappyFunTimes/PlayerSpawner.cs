639b72ba366a75d6766b2783605d6b197b34c32a
Gregg Tavares <github@greggman.com>
2014-07-15 16:01:59 -0700

    initial commit

---
+++ HappyFunTimes/PlayerSpawner.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using HappyFunTimes;
+
+namespace HappyFunTimes {
+
+public class SpawnInfo {
+    public NetPlayer netPlayer;
+    public string name;
+};
+
+[AddComponentMenu("HappyFunTimes/PlayerSpawner")]
+public class PlayerSpawner : MonoBehaviour
+{
+    public GameObject prefabToSpawnForPlayer;
+    public string gameId = "";
+    public bool showMessages = false;
+
+    void StartConnection() {
+        GameServer.Options options = new GameServer.Options();
+        options.gameId = gameId;
+
+        m_server = new GameServer(options, gameObject);
+        m_server.Init();
+
+        m_server.OnPlayerConnect += StartNewPlayer;
+        m_server.OnConnect += Connected;
+        m_server.OnDisconnect += Disconnected;
+    }
+
+    void StartNewPlayer(object sender, PlayerConnectMessageArgs e)
+    {
+        GameObject gameObject = (GameObject)Instantiate(prefabToSpawnForPlayer);
+        SpawnInfo spawnInfo = new SpawnInfo();
+        spawnInfo.netPlayer = e.netPlayer;
+        spawnInfo.name = "Player" + (++m_count);
+        gameObject.SendMessage("InitalizeNetPlayer", spawnInfo);
+    }
+
+    void Start ()
+    {
+        StartConnection();
+    }
+
+    void Connected(object sender, EventArgs e)
+    {
+    }
+
+    void Disconnected(object sender, EventArgs e)
+    {
+    }
+
+    void Cleanup()
+    {
+        if (m_server != null) {
+            m_server.Close();
+        }
+    }
+
+    void OnDestroy()
+    {
+        Cleanup();
+    }
+
+    void OnApplicationExit()
+    {
+        Cleanup();
+    }
+
+    private GameServer m_server;
+    private int m_count;
+};
+
+}   // namespace HappyFunTimes
---
+++ README.md
@@ -0,0 +1,9 @@
+HappyFunTimes-Unity3D
+=====================
+
+Unity3D libraries for HappyFunTimes.
+
+It's probably easist to download this from the asset store but otherwise you can build it from source
+here.
+
+
