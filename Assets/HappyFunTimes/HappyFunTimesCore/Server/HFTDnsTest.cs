ef605d53fdd6cb34440885dead083d6d11debf7e
Gregg Tavares <github@greggman.com>
2016-05-12 01:58:12 +0900

    add HFTDnsTest

---
+++ Assets/HappyFunTimes/HappyFunTimesCore/Server/HFTDnsTest.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using HappyFunTimes;
+
+public class HFTDnsTest : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start ()
+    {
+        Debug.Log("HFTDnsTest - start");
+        dnsRunner_ = new HFTDnsRunner();
+        dnsRunner_.Start();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+    }
+
+    void Cleanup()
+    {
+        if (dnsRunner_ != null)
+        {
+            Debug.Log("HFTDnsTest - stop");
+            dnsRunner_.Stop();
+            dnsRunner_ = null;
+        }
+    }
+
+    void OnDestroy()
+    {
+        Cleanup();
+    }
+
+    void OnApplicationExit()
+    {
+        Cleanup();
+    }
+
+    HFTDnsRunner dnsRunner_;
+}
