e9f2eebf225ab51cbe4f127b393f537b8c799ab6
Gregg Tavares <github@greggman.com>
2016-05-12 01:49:49 +0900

    make HFTDnsRunner use event processor

--- Assets/HappyFunTimes/HappyFunTimesCore/Server/HFTDNSRunner.cs
+++ Assets/HappyFunTimes/HappyFunTimesCore/Server/HFTDNSRunner.cs
@@ -4,6 +4,7 @@ using DNS.Server;
 using System.Collections.Generic;
 using System.Net;
 using System.Threading;
+using UnityEngine;
 
 namespace HappyFunTimes {
 
@@ -11,7 +12,9 @@ namespace HappyFunTimes {
     {
         public void Start()
         {
-            helper_ = new HFTDnsRunnerHelper();
+            GameObject go = new GameObject("HFTDnsEventProcessor");
+            eventProcessor_ = go.AddComponent<EventProcessor>();
+            helper_ = new HFTDnsRunnerHelper(eventProcessor_);
             dnsThread_ = new Thread(helper_.DoWork);
             dnsThread_.Start();
         }
@@ -20,28 +23,70 @@ namespace HappyFunTimes {
         {
             helper_.Close();
             dnsThread_.Join();
+            if (eventProcessor_ != null)
+            {
+                UnityEngine.Object.Destroy(eventProcessor_.gameObject);
+            }
         }
 
         Thread dnsThread_;
         HFTDnsRunnerHelper helper_;
+        EventProcessor eventProcessor_;
+    }
+
+    class HFTDnsServer : DNS.Server.DnsServer {
+        public HFTDnsServer(string ipAddressToReturnForAllDomains) : base (new DNSAll(ipAddressToReturnForAllDomains))
+        {
+        }
+
+        override protected IResponse ResolveRemote(Request request)
+        {
+            #pragma warning disable
+                throw new System.InvalidOperationException("should never get here!");
+                return null;
+            #pragma warning restore
+        }
     }
 
     class HFTDnsRunnerHelper
     {
+        public HFTDnsRunnerHelper(EventProcessor ep)
+        {
+            eventProcessor_ = ep;
+        }
+
         public void DoWork()
         {
-            dnsAll_ = new DNSAll("5.6.7.10");
-            dnsServer_ = new DNS.Server.DnsServer(dnsAll_, "9.9.9.9", 53);
-            dnsServer_.Listen(4444);
+            int port = 53;
+
+            try
+            {
+                log_.Info("DNS DoWork -start-");
+                dnsServer_ = new HFTDnsServer("5.6.7.10");
+                dnsServer_.Listen(port);
+                log_.Info("DNS DoWork -end-");
+            }
+            catch (System.Exception ex)
+            {
+                eventProcessor_.QueueEvent(() => {
+                  HFTDialog.MessageBox(
+                    "ERROR",
+                    "Could not start DNS Server on port:" + port + "\n" +
+                    "Did you run from the command line with sudo?\n\n" + ex.ToString());
+                });
+            }
         }
 
         public void Close()
         {
             dnsServer_.Close();
+            log_.Info("DNS DoWork -close-");
         }
 
         DNS.Server.DnsServer dnsServer_;
         DNSAll dnsAll_;
+        EventProcessor eventProcessor_;
+        HFTLog log_ = new HFTLog("HFTDnsRunnerHelper");
     }
 
     class DNSAll : IQuestionAnswerer
