fc92efcc0bc2a1b077129677ec2fad6cc49a647c
Gregg Tavares <github@greggman.com>
2016-05-12 01:38:45 +0900

    add HFTWebServerUtils

---
+++ Assets/HappyFunTimes/HappyFunTimesCore/Server/HFTWebServerUtils.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using WebSocketSharp;
+using WebSocketSharp.Net;
+using WebSocketSharp.Server;
+
+namespace HappyFunTimes {
+    public class HFTWebServerUtils {
+
+        public HFTWebServerUtils(string gamePath)
+        {
+            m_gamePath = gamePath;
+        }
+
+        public void SendJsonBytes(HttpListenerResponse res, byte[] data)
+        {
+            res.StatusCode = (int)HttpStatusCode.OK;
+            res.ContentType = "application/json";
+            res.ContentEncoding = System.Text.Encoding.UTF8;
+            res.AddHeader("Access-Control-Allow-Origin", "*");
+            res.WriteContent(data);
+        }
+
+        public void SendContent(HttpListenerResponse res, string path, string content)
+        {
+            SendContent(res, path, System.Text.Encoding.UTF8.GetBytes(content));
+        }
+
+        public void SendContent(HttpListenerResponse res, string path, byte[] content)
+        {
+            string mimeType = HFTMimeType.GetMimeType(path);
+            res.ContentType = mimeType;
+            if (mimeType.StartsWith("text/") ||
+                mimeType == "application/javascript")
+            {
+                res.ContentEncoding = System.Text.Encoding.UTF8;
+            }
+            res.AddHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+            res.AddHeader("Pragma",        "no-cache");                            // HTTP 1.0.
+            res.AddHeader("Expires",       "0");                                   // Proxies.
+            res.WriteContent(content);
+        }
+
+        public bool GetGameFile(string path, out byte[] content)
+        {
+            bool found = HFTWebFileDB.GetInstance().GetFile(path, out content);
+            if (!found)
+            {
+                if (path.StartsWith(m_gamePath))
+                {
+                    path = path.Substring(m_gamePath.Length - 1);
+                    found = HFTWebFileDB.GetInstance().GetFile(path, out content);
+                }
+            }
+            return found;
+        }
+
+        public void SendFile(string path, HttpListenerRequest req, HttpListenerResponse res)
+        {
+            byte[] content = null;
+            if (!GetGameFile(path, out content))
+            {
+                res.StatusCode = (int)HttpStatusCode.NotFound;
+                return;
+            }
+
+            SendContent(res, path, content);
+        }
+
+        private string m_gamePath;
+    }
+}
