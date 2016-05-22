76ff7bcc4b99baa33f01b80547022dc3792bcabf
Gregg Tavares <github@greggman.com>
2016-05-22 20:57:34 +0900

    only show readme if it's not the same scene

--- Assets/HappyFunTimes/Scripts/HFTReadmeUtils.cs
+++ Assets/HappyFunTimes/Scripts/HFTReadmeUtils.cs
@@ -6,6 +6,8 @@ namespace HappyFunTimes {
 
     public class HFTReadmeUtils {
 
+        const string kLastScene = "happyFunTimesReadmeSceneHash";
+
         #if UNITY_EDITOR
         static public void CloseReadme(Component component)
         {
@@ -39,9 +41,67 @@ namespace HappyFunTimes {
             return null;
         }
 
-        static public void ShowReadme(string name, string text, bool richText, Component component)
+        static string GetSceneHash()
+        {
+            // NOTE: I tried getting the list of loaded scenes but it's 0
+            // when I get here. But active scene seems to be valid
+            string sceneHash = "";
+            var scene = UnityEditor.SceneManagement.EditorSceneManager.GetActiveScene();
+            if (!String.IsNullOrEmpty(scene.path))
+            {
+                sceneHash = scene.path;
+            }
+            else if (!String.IsNullOrEmpty(scene.name))
+            {
+                sceneHash = scene.name;
+            }
+            return sceneHash;
+        }
+
+        // This is an attempt to not make the README too annoying.
+        // Without this, every time the scene loads (which means every time
+        // you press stop after runnign the game) the readme would re-appear
+        // It's in the way so you close it only to have it show up again
+        //
+        // With this it will only open if the user loads a new scene. Not sure
+        // that's the best but I want the readme to appear.
+        static bool IsSameSceneAsLastTime()
+        {
+            string sceneHash = GetSceneHash();
+            string oldSceneHash = PlayerPrefs.GetString(kLastScene, "");
+            if (sceneHash.Equals(oldSceneHash))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static void RememberScene()
+        {
+            PlayerPrefs.SetString(kLastScene, GetSceneHash());
+        }
+
+        static bool ShouldShowReadme()
         {
             if (Application.isPlaying)
+            {
+                return false;
+            }
+
+            bool isSameScene = IsSameSceneAsLastTime();
+            if (isSameScene)
+            {
+                return false;
+            }
+            RememberScene();
+
+            return true;
+        }
+
+        static public void ShowReadme(string name, string text, bool richText, Component component)
+        {
+            if (!ShouldShowReadme())
             {
                 return;
             }
