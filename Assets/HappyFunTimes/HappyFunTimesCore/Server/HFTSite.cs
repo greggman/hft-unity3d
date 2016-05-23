af1a0c91661e6e80d0f08d96d99a08fc711174b1
Gregg Tavares <github@greggman.com>
2016-05-23 20:41:46 +0900

    make HFTSite do both ipv4 and ipv6

--- Assets/HappyFunTimes/HappyFunTimesCore/Server/HFTSite.cs
+++ Assets/HappyFunTimes/HappyFunTimesCore/Server/HFTSite.cs
@@ -32,6 +32,105 @@ namespace HappyFunTimes
             public string port = "18679";
         }
 
+        class SharedState {
+            public bool success = false;
+        }
+
+        class Informer {
+            const int kMaxTries = 60;
+
+            string url_;
+            string domain_;
+            string addressesStr_;
+            byte[] addressesBytes_;
+            SharedState sharedState_;
+            IEnumerator informCoroutine_;
+            int tryCount_ = 0;
+            bool done_ = false;
+            bool haveNewAddresses_ = false;
+            WWW www_;
+            HFTLog log_;
+
+            public Informer(HFTLog log, SharedState sharedState, string url, string domain)
+            {
+                sharedState_ = sharedState;
+                log_ = log;
+                url_ = url;
+                domain_ = domain;
+            }
+
+            public void Inform(bool haveNewAddresses, MonoBehaviour monoBehaviour, byte[] addressBytes, string addressesStr)
+            {
+                addressesBytes_ = addressBytes;
+                addressesStr_ = addressesStr;
+                haveNewAddresses_ |= haveNewAddresses;
+
+                if (www_ == null) // no request in progress
+                {
+                    if (haveNewAddresses)
+                    {
+                        haveNewAddresses = false;
+                        tryCount_ = 0;
+                        done_ = false;
+                        sharedState_.success = false;
+                    }
+                    if (!done_)
+                    {
+                        informCoroutine_ = InformCoroutine();
+                        monoBehaviour.StartCoroutine(informCoroutine_);
+                    }
+                }
+            }
+
+            public void Stop(MonoBehaviour monoBehaviour)
+            {
+                if (informCoroutine_ != null)
+                {
+                    monoBehaviour.StopCoroutine(informCoroutine_);
+                    informCoroutine_ = null;
+                }
+            }
+
+
+            IEnumerator InformCoroutine()
+            {
+                var headers = new Dictionary<string, string>();
+                headers["Content-Type"] = "application/json";
+                headers["Host"] = domain_;
+
+                www_ = new WWW(url_, addressesBytes_, headers);
+
+                yield return www_;
+
+                string err = www_.error;
+                string result = www_.text;
+                www_ = null;
+
+                // Was it successful?
+                if (String.IsNullOrEmpty(err))
+                {
+                    // Yes
+                    log_.Info("registered: " + addressesStr_ + " with " + domain_ + " for: " + result);
+                    done_ = true;
+                    sharedState_.success = true;
+                }
+                else
+                {
+                    ++tryCount_;
+                    if (tryCount_ > 2 && !sharedState_.success)
+                    {
+                        log_.Info("Try " + tryCount_ + " of " + kMaxTries + ": Could not contact: " + domain_ +
+                                  "[" + url_ + "]\nSTATUS: " + result);
+                    }
+                    if (tryCount_ >= kMaxTries || tryCount_ > 1 && sharedState_.success)
+                    {
+                        done_ = true;
+                    }
+                }
+                informCoroutine_ = null;
+            }
+        }
+
         class InformData
         {
             public InformData(string[] _addresses, string _port)
@@ -43,10 +142,42 @@ namespace HappyFunTimes
             public string port;
         }
 
+        string MakeDirectUrl(string address)
+        {
+            return rendezvousUri_.Scheme + "://" + address + ":" + rendezvousUri_.Port + rendezvousUri_.PathAndQuery;
+        }
+
+        string[] GetIPAddresses(DNS.Protocol.RecordType type)
+        {
+            // This part sucks. Basically I don't know how to make ipv6 and ipv4 requests in .NET
+            // so my hacky workaround is to look up the addresses directly
+            // BUT, if the user specifices an address URL or a local URL I'm SOL basically
+            // I just pass them through if I can't find them
+            if (rendezvousUri_.HostNameType != System.UriHostNameType.IPv4 &&
+                rendezvousUri_.HostNameType != System.UriHostNameType.IPv6)
+            {
+                string[] addresses = HFTDnsUtils.GetIPAddresses(rendezvousUri_.DnsSafeHost, type);
+                if (addresses != null && addresses.Length > 0)
+                {
+                    return addresses;
+                }
+            }
+            return new string[] {
+                rendezvousUri_.Host,
+            };
+        }
+
         public void Init(Options options)
         {
             log_ = new HFTLog("hftsite");
             options_ = options;
+            rendezvousUri_ = new System.Uri(options_.rendezvousUrl);
+
+            string[] ipv4Addresses = GetIPAddresses(DNS.Protocol.RecordType.A);
+            string[] ipv6Addresses = GetIPAddresses(DNS.Protocol.RecordType.AAAA);
+            informers_[0] = new Informer(log_, sharedState_, ipv4Addresses.Length > 0 ? MakeDirectUrl(ipv4Addresses[0]) : null, rendezvousUri_.Host);
+            informers_[1] = new Informer(log_, sharedState_, ipv6Addresses.Length > 0 ? MakeDirectUrl(ipv6Addresses[0]) : null, rendezvousUri_.Host);
+
             checkCoroutine_ = CheckAddresses();
             StartCoroutine(checkCoroutine_);
         }
@@ -59,34 +190,53 @@ namespace HappyFunTimes
                 checkCoroutine_ = null;
             }
 
-            if (informCoroutine_ != null)
+            foreach (Informer informer in informers_)
             {
-                StopCoroutine(informCoroutine_);
-                informCoroutine_ = null;
+                informer.Stop(this);
             }
         }
 
 
+        // This runs continously, every second it checks for new addresses
+        // If they're new it starts the informers. In pumps the informers
+        // until they're finished (success or failure)
         IEnumerator CheckAddresses()
         {
             for (;;)
             {
                 string[] addresses = GetLocalIPAddresses();
-                string addressesStr = String.Join(", ", addresses);
-                if (!addressesStr.Equals(addressesStr_))
+                string newAddressesStr = String.Join(", ", addresses);
+                bool haveNewAddresses = false;
+                if (!newAddressesStr.Equals(oldAddressesStr_))
                 {
-                    inform_ = true;
-                    tryCount_ = 0;
+                    oldAddressesStr_ = newAddressesStr;
+                    var data = new InformData(addresses, options_.port);
+                    var json = Serializer.Serialize(data);
+                    addressBytes_ = System.Text.Encoding.UTF8.GetBytes(json);
+                    haveNewAddresses = true;
                 }
 
-                if (www_ == null && inform_)
+                foreach(Informer informer in informers_)
                 {
-                    informCoroutine_ = Inform();
-                    StartCoroutine(informCoroutine_);
+                    informer.Inform(haveNewAddresses, this, addressBytes_, oldAddressesStr_);
                 }
 
                 yield return new WaitForSeconds(1.0f);
             }
+        }
+
+        bool IsIpv4AddressWeCareAbout(string ipv4)
+        {
+            return true;
+        }
+
+        bool IsIpv6AddressWeCareAbout(string ipv6)
+        {
+            bool weCare = !(ipv6.StartsWith("fe80") ||
+                            ipv6.StartsWith("ff00") ||
+                            ipv6.StartsWith("fd00") ||
+                            ipv6.StartsWith("fec0"));
+            return weCare;
         }
 
         string[] GetLocalIPAddresses()
@@ -95,81 +245,26 @@ namespace HappyFunTimes
             var host = Dns.GetHostEntry(Dns.GetHostName());
             foreach (var ip in host.AddressList)
             {
-                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork && IsIpv4AddressWeCareAbout(ip.ToString()))
                 {
                     ipaddresses.Add(ip.ToString());
+                }
+                else if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6 && IsIpv6AddressWeCareAbout(ip.ToString()))
+                {
+                    ipaddresses.Add("[" + ip.ToString() + "]");
                 }
             }
             return ipaddresses.ToArray();
         }
 
-        IEnumerator Inform()
-        {
-            inform_ = false;
-            var uri = new System.Uri(options_.rendezvousUrl);
-            addresses_ = GetLocalIPAddresses();
-            addressesStr_ = String.Join(", ", addresses_);
-            var data = new InformData(addresses_, options_.port);
-            var json = Serializer.Serialize(data);
-            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(json);
-            var headers = new Dictionary<string, string>();
-            headers["Content-Type"] = "application/json";
-
-            www_ = new WWW(options_.rendezvousUrl, bytes, headers);
-
-            yield return www_;
-
-            string err = www_.error;
-            string result = www_.text;
-            www_ = null;
-
-            // Was it successful?
-            if (String.IsNullOrEmpty(err))
-            {
-                // Yes
-                log_.Info("registered: " + addressesStr_ + " with " + uri.Host + " for: " + result);
-            }
-            else
-            {
-                ++tryCount_;
-                // No
-                log_.Info("Try " + tryCount_ + " of " + maxTries_ + ": Could not contact: " + uri.Host +
-                          "\nSTATUS: " + result);
-                if (tryCount_ < maxTries_)
-                {
-                    inform_ = true;
-                }
-            }
-            informCoroutine_ = null;
-
-//            var req = new HFTHttpRequest.Options();
-//            req.content = json;
-//            req.url = options_.rendezvousUrl;
-//            string result = "";
-//            var code = HFTHttpRequest.SyncRequest(req, out result);
-//            if (code == System.Net.HttpStatusCode.OK)
-//            {
-//                log_.Info("registered: " + String.Join(", ", addresses) + " with " + uri.Host + " for: " + result);
-//            }
-//            else
-//            {
-//                int tries = 1;
-//                int maxTries = 1;
-//                log_.Info("Try " + tries + " of " + maxTries + ": Could not contact: " + uri.Host +
-//                          "\nSTATUS: " + code.ToString() + ": " + result);
-//            }
-        }
-
+        SharedState sharedState_ = new SharedState();
         IEnumerator checkCoroutine_;
-        IEnumerator informCoroutine_;
-        string[] addresses_;
-        string addressesStr_ = "";
-        WWW www_;
+        Informer[] informers_ = new Informer[2];
+        string oldAddressesStr_ = "";
+        byte[] addressBytes_;
+        System.Uri rendezvousUri_;
         Options options_;
         HFTLog log_;
-        int maxTries_ = 60;
-        int tryCount_ = 0;
-        bool inform_ = true;
     }
 
 }  // namespace HappyFunTimes
