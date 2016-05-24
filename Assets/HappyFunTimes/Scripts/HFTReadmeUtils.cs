175367a62bece98d5eeb00c90ded741bc33ce51b
Gregg Tavares <github@greggman.com>
2016-05-25 02:11:10 +0900

    try to handle light/dark theme in readme

--- Assets/HappyFunTimes/Scripts/HFTReadmeUtils.cs
+++ Assets/HappyFunTimes/Scripts/HFTReadmeUtils.cs
@@ -16,6 +16,56 @@ namespace HappyFunTimes {
         static Regex m_1HashRE = new Regex(@"^\# *(.+?)$", RegexOptions.CultureInvariant | RegexOptions.Multiline);
         static Regex m_2HashRE = new Regex(@"^\#\# *(.+?)$", RegexOptions.CultureInvariant | RegexOptions.Multiline);
         static Regex m_3HashRE = new Regex(@"^\#\#\# *(.+?)$", RegexOptions.CultureInvariant | RegexOptions.Multiline);
+        static Regex m_4HashRE = new Regex(@"^\#\#\#\# *(.+?)$", RegexOptions.CultureInvariant | RegexOptions.Multiline);
+        static Regex m_5HashRE = new Regex(@"^\#\#\#\#\# *(.+?)$", RegexOptions.CultureInvariant | RegexOptions.Multiline);
+
+        class Snippets {
+            public string tick;
+            public string tripleTick;
+            public string bold;
+            public string oneHash;
+            public string twoHash;
+            public string threeHash;
+            public string fourHash;
+            public string fiveHash;
+        }
+
+        static Snippets s_lightTheme;
+        static Snippets s_darkTheme;
+        static Snippets s_currentTheme;
+
+        static bool isDarkTheme()
+        {
+            Color c = UnityEditor.EditorStyles.label.normal.textColor;
+            return c.r > 0.5f || c.g > 0.5f || c.b > 0.5f;
+        }
+
+        HFTReadmeUtils()
+        {
+            if (s_lightTheme == null)
+            {
+                s_lightTheme = new Snippets();
+                s_darkTheme = new Snippets();
+
+                s_lightTheme.tick = "<color=blue>";
+                s_lightTheme.tripleTick = "<color=purple>";
+                s_lightTheme.bold = "<color=red>";
+                s_lightTheme.oneHash = "<size=24><b>";
+                s_lightTheme.twoHash = "<size=18><b>";
+                s_lightTheme.threeHash = "<size=14><b>";
+                s_lightTheme.fourHash = "<size=12><b>";
+                s_lightTheme.fiveHash = "<size=11><b>";
+
+                s_darkTheme.tick = "<color=cyan>";
+                s_darkTheme.tripleTick = "<color=magenta>";
+                s_darkTheme.bold = "<color=red>";
+                s_darkTheme.oneHash = "<size=24><b>";
+                s_darkTheme.twoHash = "<size=18><b>";
+                s_darkTheme.threeHash = "<size=14><b>";
+                s_darkTheme.fourHash = "<size=12><b>";
+                s_darkTheme.fiveHash = "<size=11><b>";
+            }
+        }
 
         static public void CloseReadme(Component component)
         {
@@ -32,40 +82,54 @@ namespace HappyFunTimes {
 
         static string ReplaceTripleTick(Match m)
         {
-            return "<color=purple>" + m.Groups[1].Value + "</color>";
+            return s_currentTheme.tripleTick + m.Groups[1].Value + "</color>";
         }
 
         static string ReplaceTick(Match m)
         {
-            return "<color=blue>" + m.Groups[1].Value + "</color>";
+            return s_currentTheme.tick + m.Groups[1].Value + "</color>";
         }
 
         static string ReplaceBold(Match m)
         {
-            return "<color=red><b>" + m.Groups[1].Value + "</b></color>";
+            return s_currentTheme.bold + m.Groups[1].Value + "</b></color>";
         }
 
         static string Replace1Hash(Match m)
         {
-            return "<size=24><b>" + m.Groups[1].Value + "</b></size>";
+            return s_currentTheme.oneHash + m.Groups[1].Value + "</b></size>";
         }
 
         static string Replace2Hash(Match m)
         {
-            return "<size=18><b>" + m.Groups[1].Value + "</b></size>";
+            return s_currentTheme.twoHash + m.Groups[1].Value + "</b></size>";
         }
 
         static string Replace3Hash(Match m)
         {
-            return "<size=14><b>" + m.Groups[1].Value + "</b></size>";
+            return s_currentTheme.threeHash + m.Groups[1].Value + "</b></size>";
+        }
+
+        static string Replace4Hash(Match m)
+        {
+            return s_currentTheme.fourHash + m.Groups[1].Value + "</b></size>";
+        }
+
+        static string Replace5Hash(Match m)
+        {
+            return s_currentTheme.fiveHash + m.Groups[1].Value + "</b></size>";
         }
 
         static public string MarkdownishToRichText(string markdownish)
         {
+            s_currentTheme = isDarkTheme() ? s_darkTheme : s_lightTheme;
+
             string s = markdownish.Replace("\n\n", "--EOL--").Replace("\n", " ").Replace("--EOL--", "\n\n");
             s = m_tripleTickRE.Replace(s, ReplaceTripleTick);
             s = m_tickRE.Replace(s, ReplaceTick);
             s = m_boldRE.Replace(s, ReplaceBold);
+            s = m_5HashRE.Replace(s, Replace5Hash);
+            s = m_4HashRE.Replace(s, Replace4Hash);
             s = m_3HashRE.Replace(s, Replace3Hash);
             s = m_2HashRE.Replace(s, Replace2Hash);
             s = m_1HashRE.Replace(s, Replace1Hash);
