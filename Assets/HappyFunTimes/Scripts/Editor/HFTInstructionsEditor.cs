014d3e343b62ed0bdc9224fb2a56ce1780d5c10f
Gregg Tavares <github@greggman.com>
2016-05-25 03:20:28 +0900

    try helpbox

--- Assets/HappyFunTimes/Scripts/Editor/HFTInstructionsEditor.cs
+++ Assets/HappyFunTimes/Scripts/Editor/HFTInstructionsEditor.cs
@@ -18,7 +18,9 @@ public class HFTInstructionsEditor : Editor {
         style.richText = true;
         style.normal.background = m_dark;
         style.normal.textColor = Color.white;
-        GUILayout.Label("You can set these from the command line with <color=ctan>--hft-instructions=\"your instructions\"</color> and <color=cyan>--hft-instuctions-bottom</color>\n\nOr of course you can design your own interface", style);
+        string help = "You can set these from the command line with <color=ctan>--hft-instructions=\"your instructions\"</color> and <color=cyan>--hft-instuctions-bottom</color>\n\nOr of course you can design your own interface";
+//        EditorGUI.HelpBox(new Rect(0, 0, 100, 100), help, MessageType.Info);
+        GUILayout.Label(help, style);
         GUILayout.Space(5);
         DrawDefaultInspector();
     }
