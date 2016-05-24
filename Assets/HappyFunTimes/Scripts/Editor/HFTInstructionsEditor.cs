6ba283565d119fd0405e1c8f5f86f8cdac2ca3d8
Gregg Tavares <github@greggman.com>
2016-05-25 03:33:56 +0900

    use HelpBox for Instructions

--- Assets/HappyFunTimes/Scripts/Editor/HFTInstructionsEditor.cs
+++ Assets/HappyFunTimes/Scripts/Editor/HFTInstructionsEditor.cs
@@ -5,22 +5,19 @@ using System.Collections;
 [CustomEditor(typeof(HappyFunTimes.HFTInstructions))]
 [CanEditMultipleObjects]
 public class HFTInstructionsEditor : Editor {
-    Texture2D m_dark;
-
-    public void OnEnable()
-    {
-        m_dark = HappyFunTimes.HFTUtil.MakeColor(new Color(0.2f, 0.2f, 0.2f));
-    }
 
     public override void OnInspectorGUI() {
-        GUIStyle style = new GUIStyle(GUI.skin.GetStyle("Label"));
-        style.wordWrap = true;
-        style.richText = true;
-        style.normal.background = m_dark;
-        style.normal.textColor = Color.white;
-        string help = "You can set these from the command line with <color=ctan>--hft-instructions=\"your instructions\"</color> and <color=cyan>--hft-instuctions-bottom</color>\n\nOr of course you can design your own interface";
-//        EditorGUI.HelpBox(new Rect(0, 0, 100, 100), help, MessageType.Info);
-        GUILayout.Label(help, style);
+        string help =
+@"You can set these from the command line with
+
+--hft-instructions=""your instructions""
+
+and
+
+--hft-instuctions-bottom
+
+Leave 'show' unchecked except for testing.";
+        EditorGUILayout.HelpBox(help, MessageType.Info);
         GUILayout.Space(5);
         DrawDefaultInspector();
     }
