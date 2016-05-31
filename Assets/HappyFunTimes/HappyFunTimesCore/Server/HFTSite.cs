68bcac100d3ff7f1a46c1dd19e13a0f6249b0655
Gregg Tavares <github@greggman.com>
2016-05-31 16:28:56 +0900

    make HFTSite use HFTIpUtils.GetLocalIPAddresses

--- Assets/HappyFunTimes/HappyFunTimesCore/Server/HFTSite.cs
+++ Assets/HappyFunTimes/HappyFunTimesCore/Server/HFTSite.cs
@@ -195,7 +195,7 @@ namespace HappyFunTimes
         {
             for (;;)
             {
-                string[] addresses = GetLocalIPAddresses();
+                string[] addresses = HFTIpUtils.GetLocalIPAddresses();
                 string newAddressesStr = String.Join(", ", addresses);
                 bool haveNewAddresses = false;
                 if (!newAddressesStr.Equals(oldAddressesStr_))
@@ -216,37 +216,6 @@ namespace HappyFunTimes
             }
         }
 
-        bool IsIpv4AddressWeCareAbout(string ipv4)
-        {
-            return true;
-        }
-
-        bool IsIpv6AddressWeCareAbout(string ipv6)
-        {
-            bool weCare = !(ipv6.StartsWith("fe80") ||
-                            ipv6.StartsWith("ff00") ||
-                            ipv6.StartsWith("fd00") ||
-                            ipv6.StartsWith("fec0"));
-            return weCare;
-        }
-
-        string[] GetLocalIPAddresses()
-        {
-            List<string> ipaddresses = new List<string>();
-            var host = Dns.GetHostEntry(Dns.GetHostName());
-            foreach (var ip in host.AddressList)
-            {
-                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork && IsIpv4AddressWeCareAbout(ip.ToString()))
-                {
-                    ipaddresses.Add(ip.ToString());
-                }
-                else if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6 && IsIpv6AddressWeCareAbout(ip.ToString()))
-                {
-                    ipaddresses.Add("[" + ip.ToString() + "]");
-                }
-            }
-            return ipaddresses.ToArray();
-        }
 
         SharedState sharedState_ = new SharedState();
         IEnumerator checkCoroutine_;
