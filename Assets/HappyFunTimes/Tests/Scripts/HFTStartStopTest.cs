ac50a575a54b414c953f5517d35ccefaf246e495
Gregg Tavares <github@greggman.com>
2016-06-03 20:23:29 +0900

    add HFTStartStopTest

---
+++ Assets/HappyFunTimes/Tests/Scripts/HFTStartStopTest.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace HappyFunTimes
+{
+    public class HFTStartStopTest : MonoBehaviour
+    {
+        PlayerSpawner m_spawner;
+        bool started = false;
+
+        // Use this for initialization
+        void Start ()
+        {
+            m_spawner = GetComponent<PlayerSpawner>();
+        }
+
+        void OnGUI()
+        {
+            if (GUI.Button(new Rect(10, 10, 150, 100), started ? "Stop HFT" : "Start HFT"))
+            {
+                if (started)
+                {
+                    m_spawner.StopHappyFunTimes();
+                }
+                else
+                {
+                    m_spawner.StartHappyFunTimes();
+                }
+                started = !started;
+            }
+
+        }
+    }
+}
+
